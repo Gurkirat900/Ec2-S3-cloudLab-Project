5ab12a53eeed5c5003aebf1cc74fa76dca54a357
Gurkirat900 <gs1683676@gmail.com>
2026-02-18 02:58:19 +0530

    downgraded node version for atlas connection string as it was giving srv issues

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB= async ()=>{
     try {
-        console.log(process.env.MONGODB_URL)
-        const connectionInstance= await mongoose.connect(process.env.MONGODB_URL)
+        const connectionInstance= await mongoose.connect(process.env.MONGODB_URL);
         console.log("MongoDB connection successful")
         console.log(`MongoDb connection sucessful at HOST- ${connectionInstance.connection.host}`)  // there are seprate database for deployment and devleopment hence multiple host
     } catch (error) {
