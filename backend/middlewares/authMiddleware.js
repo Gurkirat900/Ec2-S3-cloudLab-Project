175eddeb9f5a5db48d4edec714855c427a2bcc35
Gurkirat900 <gs1683676@gmail.com>
2026-02-19 02:32:26 +0530

    testing success

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -7,17 +7,13 @@ import jwt from "jsonwebtoken"
 export const verifyJWT= asyncHandler(async (req,res,next)=>{
    try {
      const token= req.cookies?.token || req.header("Authorization")?.replace("Bearer ","")
-     console.log("Token from request:",token)
-     console.log("Raw header:", req.header("Authorization"));
-
- 
+    
      if(!token){
          console.log("Unauthorised request")
          throw new ApiError(401,"Unauthorised request")
      }
  
      const decodedToken= jwt.verify(token,process.env.JWT_SECRET_KEY)
-        console.log("Decoded token:", decodedToken)
      const user= await User.findById(decodedToken?._id).select('_id name email role')
     
      if(!user){
