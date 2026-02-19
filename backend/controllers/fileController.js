2356b53758d66b6fa2cfcfa4fd418d01a40d2d4f
Gurkirat900 <gs1683676@gmail.com>
2026-02-19 21:31:10 +0530

    added routes to download a file and get all versions of a file

--- backend/controllers/fileController.js
+++ backend/controllers/fileController.js
@@ -3,7 +3,6 @@ import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import s3 from "../config/s3.js";
-import { version } from "mongoose";
 
 const uploadFile= asyncHandler( async (req,res)=>{
     if(!req.file){
@@ -83,4 +82,58 @@ const deleteFile= asyncHandler( async (req,res)=>{  // only admin can delete any
     )
 })
 
-export { uploadFile, getFiles, deleteFile }
+const getFileVerssions= asyncHandler( async (req,res)=>{
+    const fileId= req.params.fileId;
+    const file= await File.findById(fileId)
+    if(!file){
+        throw new ApiError(404,"File not found")
+    }
+    // only admin and the user who uploaded the file can view its versions
+    if(file.uploadedBy.toString() !== req.user._id.toString() && req.user.role !== "admin"){
+        throw new ApiError(403,"You do not have permission to view this file versions")
+    }
+
+    // Fetch file versions from S3
+    const data = await s3.listObjectVersions({
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Prefix: file.s3Key,
+    }).promise();
+
+    const versions= data.Versions?.filter(version => version.Key === file.s3Key).map(version => ({
+        VersionId: version.VersionId,
+        LastModified: version.LastModified,
+        IsLatest: version.IsLatest,
+        Size: version.Size,
+    })) || [];
+    
+    return res.status(200).json(
+        new ApiResponse(200, versions, "File versions fetched successfully")
+    )
+})
+
+const dowmloadFile= asyncHandler( async (req,res)=>{
+    const fileId= req.params.fileId;
+    const file= await File.findById(fileId)
+    if(!file){
+        throw new ApiError(404,"File not found")
+    }
+    // only admin and the user who uploaded the file can download it
+    if(file.uploadedBy.toString() !== req.user._id.toString() && req.user.role !== "admin"){
+        throw new ApiError(403,"You do not have permission to download this file")
+    }
+
+    const s3Object= await s3.getObject({
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: file.s3Key,
+    }).promise()
+
+    // set headers
+    res.setHeader("Content-Disposition", `attachment; filename="${file.filename}"`)
+    res.setHeader("Content-Type", s3Object.ContentType)
+
+    return res.status(200).send(s3Object.Body).json(
+        new ApiResponse(200,null,"File downloaded successfully")
+    )
+})
+
+export { uploadFile, getFiles, deleteFile, getFileVerssions, dowmloadFile}
--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { uploadFile, getFiles, deleteFile } from "../controllers/fileController.js";
+import { uploadFile, getFiles, deleteFile, dowmloadFile, getFileVerssions } from "../controllers/fileController.js";
 import { verifyJWT } from "../middlewares/authMiddleware.js";
 import { upload } from "../middlewares/multerMiddleware.js";
 
@@ -8,5 +8,7 @@ const router= Router()
 router.post("/upload",verifyJWT,upload.single("file"),uploadFile)  // file is the name of the field in the form-data
 router.get("/",verifyJWT,getFiles)
 router.delete("/:fileId",verifyJWT,deleteFile)  // only admin can delete any file, user can delete only their own files
+router.get("/download/:fileId",verifyJWT,dowmloadFile) // to download file from s3 bucket
+router.get("/versions/:fileId",verifyJWT,getFileVerssions) // to get all versions of a file
 
 export default router;
